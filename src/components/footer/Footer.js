06099fef48b27779e42220027ba4d30702fd7758
Bhavesh Bhimte <jammerking96@gmail.com>
2021-02-25 15:17:34 +0530

    Initial Commit for testing background

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <nav>Logo</nav>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About me</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <span>Logo</span>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About me</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
